844bc655263154fb50ba887b4eb0652590a48c23
zkang <zhangkang0324@live.cn>
2021-11-23 21:54:24 +0800

    zkang, s新闻评论

--- NewsPublish.Service/NewsService.cs
+++ NewsPublish.Service/NewsService.cs
@@ -204,6 +204,106 @@ namespace NewsPublish.Service {
             return response;
         }
 
+        /// <summary>
+        /// 查询新闻列表
+        /// </summary>
+        /// <param name="where"></param>
+        /// <param name="topCount"></param>
+        /// <returns></returns>
+        public ResponseModel GetNewsList(Expression<Func<News, bool>> where, int topCount) {
+            var list = _db.News.Include("NewsClassify").Include("NewsComment").Where(where).OrderByDescending(c => c.PublishDate).Take(topCount);
+            var response = new ResponseModel { code = 200, result = "新闻列表获取成功" };
+            response.data = new List<NewsModel>();
+            foreach (var model in list) {
+                response.data.Add(new NewsModel {
+                    Id = model.Id,
+                    ClassifyName = model.NewsClassify.Name,
+                    Title = model.Title,
+                    Image = model.Image,
+                    Contents = model.Contents.Length > 50 ? model.Contents.Substring(0, 50) : model.Contents,
+                    PublishDate = model.PublishDate.ToString("yyyy-MM-dd"),
+                    CommonCount = model.NewsComments.Count(),
+                    Remark = model.Remark
+                });
+            }
+            return response;
+        }
+
+        /// <summary>
+        /// 获取最新评论的新闻集合
+        /// </summary>
+        /// <param name="where"></param>
+        /// <param name="topCount"></param>
+        /// <returns></returns>
+        public ResponseModel GetNewComment(Expression<Func<News, bool>> where, int topCount) {
+            var newsIds = _db.NewsComment.OrderByDescending(c => c.AddTime).GroupBy(c => c.NewsId).Select(c => c.Key).Take(topCount);
+            var list = _db.News.Include("NewsClassify").Include("NewsComment").Where(c => newsIds.Contains(c.Id)).OrderByDescending(c => c.PublishDate).Take(topCount);
+            var response = new ResponseModel { code = 200, result = "最新评论的新闻列表获取成功" };
+            response.data = new List<NewsModel>();
+            foreach (var model in list) {
+                response.data.Add(new NewsModel {
+                    Id = model.Id,
+                    ClassifyName = model.NewsClassify.Name,
+                    Title = model.Title,
+                    Image = model.Image,
+                    Contents = model.Contents.Length > 50 ? model.Contents.Substring(0, 50) : model.Contents,
+                    PublishDate = model.PublishDate.ToString("yyyy-MM-dd"),
+                    CommonCount = model.NewsComments.Count(),
+                    Remark = model.Remark
+                });
+            }
+            return response;
+        }
+
+        /// <summary>
+        /// 搜索一个新闻
+        /// </summary>
+        /// <param name="where"></param>
+        /// <returns></returns>
+        public ResponseModel GetSearchOneNews(Expression<Func<News, bool>> where) {
+            var news = _db.News.Where(where).FirstOrDefault();
+            if (news == null)
+                return new ResponseModel { code = 0, result = "新闻搜索失败" };
+            return new ResponseModel { code = 200, result = "新闻搜索成功", data = news.Id };
+        }
+
+        /// <summary>
+        /// 获取新闻数量
+        /// </summary>
+        /// <param name="where"></param>
+        /// <returns></returns>
+        public ResponseModel GetNewsCount(Expression<Func<News, bool>> where) {
+            var count = _db.News.Where(where).Count();
+            return new ResponseModel { code = 200, result = "新闻数量获取成功", data = count };
+        }
+
+        /// <summary>
+        /// 获取推荐新闻列表
+        /// </summary>
+        /// <param name="newsId"></param>
+        /// <returns></returns>
+        public ResponseModel GetRecommendNewsList(int newsId) {
+            var news = _db.News.FirstOrDefault(c => c.Id == newsId);
+            if (news == null)
+                return new ResponseModel { code = 0, result = "新闻不存在" };
+            var newsList = _db.News.Include("NewsComment").Where(c => c.NewsClassifyId == news.NewsClassifyId && c.Id != newsId).OrderByDescending(c => c.PublishDate).OrderByDescending(c => c.NewsComments.Count).Take(6).ToList();
+            var response = new ResponseModel { code = 200, result = "推荐新闻列表获取成功" };
+            response.data = new List<NewsModel>();
+            foreach (var n in newsList) {
+                response.data.Add(new NewsModel {
+                    Id = n.Id,
+                    ClassifyName = n.NewsClassify.Name,
+                    Title = n.Title,
+                    Image = n.Image,
+                    Contents = n.Contents.Length > 50 ? n.Contents.Substring(0, 50) : n.Contents,
+                    PublishDate = n.PublishDate.ToString("yyyy-MM-dd"),
+                    CommonCount = n.NewsComments.Count(),
+                    Remark = n.Remark
+                });
+            }
+            return response;
+        }
+
         #endregion
     }
 }
