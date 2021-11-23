ec6ece6d17dc1545e323e1f66f3320ff6d571a11
zkang <zhangkang0324@live.cn>
2021-11-24 00:14:54 +0800

    后台管理页面

---
+++ NewsPublish.Web/Areas/Admin/Controllers/BannerController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NewsPublish.Service;
+
+namespace NewsPublish.Web.Areas.Admin.Controllers {
+    [Area("Admin")]
+    public class BannerController : Controller {
+
+        private BannerService _bannerService;
+        public BannerController(BannerService bannerService) {
+            this._bannerService = bannerService;
+        }
+
+        // GET: BannerController
+        public ActionResult Index() {
+            var banner = _bannerService.GetBannerList();
+            return View(banner);
+        }
+
+        // GET: BannerController/Details/5
+        public ActionResult Details(int id) {
+            return View();
+        }
+
+        // GET: BannerController/Create
+        public ActionResult Create() {
+            return View();
+        }
+
+        // POST: BannerController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection) {
+            try {
+                return RedirectToAction(nameof(Index));
+            }
+            catch {
+                return View();
+            }
+        }
+
+        // GET: BannerController/Edit/5
+        public ActionResult Edit(int id) {
+            return View();
+        }
+
+        // POST: BannerController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection) {
+            try {
+                return RedirectToAction(nameof(Index));
+            }
+            catch {
+                return View();
+            }
+        }
+
+        // GET: BannerController/Delete/5
+        public ActionResult Delete(int id) {
+            return View();
+        }
+
+        // POST: BannerController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection) {
+            try {
+                return RedirectToAction(nameof(Index));
+            }
+            catch {
+                return View();
+            }
+        }
+    }
+}
--- NewsPublish.Web/Startup.cs
+++ NewsPublish.Web/Startup.cs
@@ -4,37 +4,35 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using NewsPublish.Service;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace NewsPublish.Web
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
+namespace NewsPublish.Web {
+    public class Startup {
+        public Startup(IConfiguration configuration) {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
+        public void ConfigureServices(IServiceCollection services) {
             services.AddControllersWithViews();
+            services.AddTransient<BannerService>();
+            services.AddTransient<CommentService>();
+            services.AddTransient<DB>();
+            services.AddTransient<NewsService>();
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+            if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
-            }
-            else
-            {
+            } else {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
@@ -46,8 +44,10 @@ namespace NewsPublish.Web
 
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
+            app.UseEndpoints(endpoints => {
+                endpoints.MapControllerRoute(
+                   name: "areaRoute",
+                   pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
